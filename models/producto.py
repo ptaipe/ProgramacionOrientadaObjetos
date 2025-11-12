726d5b907fd16690ca842cf5c43eaee41b885144
profesorpaulotaipe-creator <profesorpaulotaipe@gmail.com>
2025-11-12 19:46:19 -0300

    modelo de producto creado

---
+++ models/producto.py
@@ -0,0 +1,10 @@
+class Producto:
+
+    def __init__(self, id_producto, imagen, nombre, descripcion, precio, stock, estado):
+        self.id_producto = id_producto
+        self.imagen = imagen
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.precio = precio
+        self.stock = stock
+        self.estado = estado
