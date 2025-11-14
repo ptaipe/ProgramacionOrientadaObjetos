d2967a9d5a2890ff5a83d10035f2e3bf47091ba8
profesorpaulotaipe-creator <profesorpaulotaipe@gmail.com>
2025-11-14 20:22:18 -0300

    cliente_controller terminado

--- api/controllers/cliente_controller.py
+++ api/controllers/cliente_controller.py
@@ -24,3 +24,21 @@ def crear():
 @cliente_api.get("/clientes/<int:id_cliente>")
 def buscar(id_cliente):
     return jsonify(service.buscar_por_id(id_cliente))
+
+@cliente_api.put("/clientes/<int:id_cliente>")
+def actualizar(id_cliente):
+    data = request.json
+    cliente = Cliente(
+        id_cliente = id_cliente,
+        nombre = data["nombre"],
+        apellido = data["apellido"],
+        correo = data["correo"],
+        telefono=data["telefono"]
+    )
+    service.actualizar(cliente)
+    return jsonify({"msg": "Cliente modificado con Exito."})
+
+@cliente_api.delete("/clientes/<int:id_cliente>")
+def eliminar(id_cliente):
+    service.eliminar(id_cliente)
+    return jsonify({"msg": "Cliente eliminado con Exito."})
